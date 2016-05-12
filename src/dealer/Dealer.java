7eb2bd4a927fe9c07808d311be7ee0441cc63b9d
yonghenglh6 <yonghenglh6@163.com>
2016-05-12 20:26:46 +0800

    Dealer First

---
+++ src/dealer/Dealer.java
@@ -0,0 +1,22 @@
+package dealer;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Dealer extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getOutputStream().print("{Die Now.}");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getOutputStream().print("{Die Now.Post}");
+	}
+}
